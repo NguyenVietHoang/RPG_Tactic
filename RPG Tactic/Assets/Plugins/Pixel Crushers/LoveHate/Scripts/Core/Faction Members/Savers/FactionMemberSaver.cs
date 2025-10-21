dfeb048d435460cc3bcc36deb5aca44ef64b23cb
Viet Hoang NGUYEN <nvhoang.nick@gmail.com>
2025-10-21 21:01:47 +0700

    + Project Init

---
+++ RPG Tactic/Assets/Plugins/Pixel Crushers/LoveHate/Scripts/Core/Faction Members/Savers/FactionMemberSaver.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Pixel Crushers. All rights reserved.
+
+using UnityEngine;
+
+namespace PixelCrushers.LoveHate
+{
+
+    /// <summary>
+    /// Saves a FactionMember's data to the SaveSystem.
+    /// </summary>
+    [AddComponentMenu("")] // Use wrapper instead.
+    [RequireComponent(typeof(FactionMember))]
+    public class FactionMemberSaver : Saver
+    {
+
+        private FactionMember m_member = null;
+
+        public override void Awake()
+        {
+            base.Awake();
+            m_member = GetComponent<FactionMember>();
+        }
+
+        public override string RecordData()
+        {
+            return m_member.SerializeToString();
+        }
+
+        public override void ApplyData(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return;
+            m_member.DeserializeFromString(s);
+        }
+
+    }
+}
